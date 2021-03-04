ec3b82aeed761815cf0f9c833a2845abb59e26ac
kotlaki <voland171@mail.ru>
2021-03-04 16:59:14 +0500

    Работаем над SpringBootTest

---
+++ server-web-api/src/test/java/com/kurganov/webserver/UTest.java
@@ -0,0 +1,23 @@
+//package com.kurganov.webserver;
+//
+//import com.kurganov.webserver.config.AppConfigWeb;
+//import com.kurganov.webserver.utils.ShoppingCart;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = {AppConfigWeb.class})
+//public class UTest {
+//
+//    @Autowired
+//    private ShoppingCart shoppingCart;
+//
+//    @Test
+//    public void addProductCart() {
+//        shoppingCart.add(1L);
+//    }
+//
+//}
